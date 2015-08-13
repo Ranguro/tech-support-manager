bed675afd4f09b978ba7f2bd3246d91e343768f0
lesmedGutierrez <daesxz@live.com>
2015-08-12 22:42:47 -0600

    Initial commit

---
+++ app/src/main/java/com/example/ranguro/technicalsupportmanager/SignInFragment.java
@@ -0,0 +1,25 @@
+package com.example.ranguro.technicalsupportmanager;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SignInFragment extends Fragment {
+
+    private final String LOG_TAG = SignInFragment.class.getSimpleName();
+
+
+    public SignInFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sign_in, container, false);
+    }
+}
