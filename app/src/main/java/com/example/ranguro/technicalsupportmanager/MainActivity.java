7917872bca1b62d6e7a151db49ae62db445ca5aa
Ranguro <randy1002@gmail.com>
2015-08-20 13:57:55 -0600

    Add: Accoun Log In and Sign up

--- app/src/main/java/com/example/ranguro/technicalsupportmanager/MainActivity.java
+++ app/src/main/java/com/example/ranguro/technicalsupportmanager/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int x = 0;
+        int x = 1;
     }
 
 
