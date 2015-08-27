2be845b076079fc3a283d1d65c48205c675982dd
Jonathan Solís Parajeles <jonathan.solis.parajeles@gmail.com>
2015-08-27 17:11:42 -0600

    Added the account for the signin. Plus validation for phone number

--- app/src/main/java/com/example/ranguro/technicalsupportmanager/SignUpActivity.java
+++ app/src/main/java/com/example/ranguro/technicalsupportmanager/SignUpActivity.java
@@ -1,16 +1,56 @@
 package com.example.ranguro.technicalsupportmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SignUpActivity extends AppCompatActivity {
+    private EditText firstname;
+    private EditText lastname;
+    private EditText email;
+    private EditText phone;
+    private Spinner accessType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        firstname = (EditText)findViewById(R.id.field_firstname);
+        lastname = (EditText)findViewById(R.id.field_lastname);
+        email = (EditText)findViewById(R.id.field_email);
+        phone = (EditText)findViewById(R.id.field_phone);
+        accessType = (Spinner)findViewById(R.id.field_access_type);
+        phone.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                List<String> invalidInputs = new ArrayList<String>();
+                invalidInputs.add("70");
+                invalidInputs.add("69");
+                invalidInputs.add("76");
+                invalidInputs.add("56");
+                invalidInputs.add("59");
+                boolean result = invalidInputs.contains(String.valueOf(keyCode));
+                if((phone.getText().length() == 8 && keyCode != 67) || result){
+                    keyCode = 0;
+                    return true;
+                }
+                return false;
+            }
+        });
+
     }
 
 
@@ -34,5 +74,28 @@ public class SignUpActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+    public void nextSignUp(View view) {
+        String firstNameText = firstname.getText().toString();
+        String lastNameText = lastname.getText().toString();
+        String emailText = email.getText().toString();
+        String phoneText = phone.getText().toString();
+        String accessTypeText = accessType.getSelectedItem().toString();
+        if (firstNameText.equals("") || lastNameText.equals("") || emailText.equals("") || phoneText.equals("")) {
+            Toast.makeText(this, "Complete all the fields", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Intent nextSignUpIntent = new Intent(this, NextSignUpActivity.class);
+            nextSignUpIntent.putExtra(getString(R.string.firstname), firstNameText);
+            nextSignUpIntent.putExtra(getString(R.string.lastname), lastNameText);
+            nextSignUpIntent.putExtra(getString(R.string.email), emailText);
+            nextSignUpIntent.putExtra(getString(R.string.phone), phoneText);
+            nextSignUpIntent.putExtra(getString(R.string.accessType), accessTypeText);
+            startActivity(nextSignUpIntent);
+        }
+
     }
 }
