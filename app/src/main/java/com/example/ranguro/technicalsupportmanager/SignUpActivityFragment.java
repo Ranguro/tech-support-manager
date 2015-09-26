0beb0c99f00d657d0558cb814c425097c0697a03
Jonathan Solís Parajeles <jonathan.solis.parajeles@gmail.com>
2015-09-25 19:55:51 -0600

    New icon, and titles at activities

---
+++ app/src/main/java/com/example/ranguro/technicalsupportmanager/SignUpActivityFragment.java
@@ -0,0 +1,24 @@
+package com.example.ranguro.technicalsupportmanager;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SignUpActivityFragment extends Fragment {
+
+    private final String LOG_TAG = SignUpActivityFragment.class.getSimpleName();
+
+    public SignUpActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+    }
+}
