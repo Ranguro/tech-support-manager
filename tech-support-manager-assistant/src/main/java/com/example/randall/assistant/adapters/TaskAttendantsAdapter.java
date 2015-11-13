7da2218e766234e04911e3ac869bdacaf699bbc1
Ranguro <randy1002@gmail.com>
2015-11-12 20:47:49 -0600

    feat: Added GCM to the manifest on each module.

--- tech-support-manager-assistant/src/main/java/com/example/randall/assistant/adapters/TaskAttendantsAdapter.java
+++ tech-support-manager-assistant/src/main/java/com/example/randall/assistant/adapters/TaskAttendantsAdapter.java
@@ -26,6 +26,7 @@ public class TaskAttendantsAdapter extends  RecyclerView.Adapter<TaskAttendantsA
 
     public TaskAttendantsAdapter() {
 
+
     }
 
     public void addAll(List<ParseObjectUser> taskAttendantsList) {
